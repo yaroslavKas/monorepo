c9555e4a6ed85230b075c1e901c45fde40d85820
kas27 <bibizyana27@gmai.com>
2022-10-26 18:39:23 +0300

    check husky

--- packages/front/src/App.tsx
+++ packages/front/src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import './App.css';
-import Button from "@monorepo/button";
-import Input from "@monorepo/input";
-import {Icon, SvgIcon} from "@monorepo/icons";
-
+import Button from '@monorepo/button';
+import Input from '@monorepo/input';
+import { Icon, SvgIcon } from '@monorepo/icons';
 
 function App() {
-  return (
-    <div className="App">
-        <Button content={"lodfkdslfksd"}/>
-        <Input/>
-        <Icon name="delete_0" />
-        <Icon name="error_0" />
-        <Icon name="function_0" />
-        <Icon name="shared_0" />
-        <SvgIcon name="function_0" />
-        <SvgIcon name="plus_0" />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Button content={'lodfkdslfksd'} />
+            <Input />
+            <Icon name="delete_0" />
+            <Icon name="error_0" />
+            <Icon name="function_0" />
+            <Icon name="shared_0" />
+            <SvgIcon name="function_0" />
+            <SvgIcon name="plus_0" />
+        </div>
+    );
 }
 
 export default App;
